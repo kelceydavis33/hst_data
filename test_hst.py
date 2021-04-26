bae94990920825e1b8427244d3ea5e836e4e5a56
kelcey.davis33 <kdavis@inrainbows.sdsu.edu>
2021-04-26 12:13:13 -0700

    clearing images

--- test_hst.py
+++ test_hst.py
@@ -3,29 +3,90 @@
 
 # In[ ]:
 
-
+from astropy.coordinates import FK4
+import astropy.units as u
+from astropy.io import fits
+from astropy.wcs import WCS
 import matplotlib.pyplot as plt
 import glob
 import numpy as np
 get_ipython().run_line_magic('matplotlib', 'inline')
-from astropy import units as u
 from astropy.coordinates import SkyCoord
-from deepCR import deepCR as dcr
-from astropy.io import fits
+from deepCR import deepCR 
 from astropy.visualization import astropy_mpl_style
 from astropy.utils.data import get_pkg_data_filename
-from astropy.io import fits
-file_path = input('What is the absolute path to the data files?')
-thresh = input('What threshold? 0.5 reccomended.')
-image = fits.getdata(file_path)[:512,:512]
-mdl = deepCR(mask="ACS-WFC-F606W-2-32", inpaint="ACS-WFC-F606W-2-32", device="CPU")
-mask, cleaned_image = mdl.clean(image, threshold = thresh)
-prob_mask = mdl.clean(image, binary=False)
-plt.imshow(cleaned_image)
-plt.title('Adjusted Image')
-plt.savefig('adjusted.png')
-plt.fig()
-plt.imshow(image)
-plt.title('Original Image')
-plt.savefig('orig.png')
+plt.rcParams['figure.figsize'] = (20, 10)
+obj_path = input('What is the path to the known objects in a .txt file?')
+sav_path = input('Directory where the files should be stored:')
+file_path= input('What is the path to the files? Include an identifier or asteriks wildcard identifier.')
+paths = glob.glob(file_path)
+length = len(paths)
+thresh = float(input('What threshold would you like to use? 0.5 reccomended.'))
+lost = []
+n = 0
+bad_im = 0
+for path in paths:
+    n +=1
+    print(f'Processing image {n} of {length}')
+    #try:
+    image = fits.getdata(path)
+    names = ['raQSO','decQSO']
+    dtype = []
+    dtype.append( (raQSO,U20) )
+    dtype.append( (decQSO,U20) )
+    gotoqinfo = np.genfromtxt(obj_path,dtype=dtype)
+    coords = []
+    for i in len(gotoqinfo.shape[0]):
+        c = SkyCoord(gotoqinfo[i][0], gotoqinfo[i][1], frame = FK4, unit = (u.hourangle, u.deg))
+        coords.append(c)
+    with fits.open(path) as hdu:
+        wcs = WCS(fobj = hdu[1], header = hdu[1].header)
+        pix_x = []
+        pix_y = []
+        for coord in coords:
+            ra = coord.ra
+            dec = coord.dec
+            y, x = wcs.all_world2pix(ra, dec, 1, adaptive = False, ra_dec_order = True)
+            pix_x.append(x)
+            pix_y.append(y)
+    mdl = deepCR(mask = "ACS-WFC-F606W-2-32", inpaint = "ACS-WFC-F606W-2-32", device = "CPU")
+    mask, cleaned_image = mdl.clean(image, threshold = thresh)
+    vmax = np.median(cleaned_image)+5*np.std(cleaned_image)
+    vmin = np.median(cleaned_image)-5*np.std(cleaned_image)
+    figs, axs = plt.subplots(1,4)
+    axs[0].imshow(cleaned_image, vmin = vmin, vmax = vmax, origin = 'lower', cmap = 'gray')
+    ident = path.split('/')[-1].split('.')[0]
+    axs[0].set_title(f'Adjusted')
+   # plt.savefig(sav_path + f'{ident}_adjusted.png');
+    #plt.figure()
+    #vmx = np.median(image)+5*np.std(image)
+    #vmn = np.median(image)-5*np.std(image)
+    axs[1].imshow(image, vmin = vmin, vmax = vmax, origin = 'lower', cmap = 'gray')
+    axs[1].set_title(f'Original Image')
+    axs[2].imshow(mask, origin = 'lower', cmap = 'gray')
+    axs[2].set_title(f'ProbabilityMask')
+    axs[3].scatter(pix_x, pix_y)
+    #axs[3].set_xlim(0,
+    #axs[3].set_ylim(0, 
+    axs[3].set_title('Known Stars')
+   # plt.show()
+    plt.savefig(sav_path + f'{ident}.png')
+   # plt.savefig(sav_path + f'{ident}_original');
+   # except:
+    #    bad_im +=1
+     #   lost.append(path.split('/')[-1].split('.')[0])
+#print(f'{bad_im} images not processed.The following files were not processed:{lost}')
+#file_path = input('What is the absolute path to the data files?')
+#thresh = input('What threshold? 0.5 reccomended.')
+#image = fits.getdata(file_path)[:512,:512]
+#mdl = deepCR(mask="ACS-WFC-F606W-2-32", inpaint="ACS-WFC-F606W-2-32", device="CPU")
+#mask, cleaned_image = mdl.clean(image, threshold = thresh)
+#prob_mask = mdl.clean(image, binary=False)
+#plt.imshow(cleaned_image)
+#plt.title('Adjusted Image')
+#plt.savefig(f'adjusted_thresh{thresh}.png')
+#plt.fig()
+#plt.imshow(image)
+#plt.title('Original Image')
+#plt.savefig('orig.png')
 
